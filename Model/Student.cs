c9dd2b6c466a22a92969eb98e0b5f68634fa1720
timnguyen90 <33167258+timnguyen90@users.noreply.github.com>
2018-07-15 20:54:18 +0700

    Update Employee and Student

--- Model/Student.cs
+++ Model/Student.cs
@@ -1,5 +1,15 @@
 public class Student{
 	public Id {get; set;}
 	public Fullname {get; set;}
+	public Address {get; set;}
+	public PhoneNo {get; set;}
 	
+	public Student(){}
+	
+	public Student(Id, Fullname,Address,PhoneNo){
+		this.Id = Id;
+		this.Fullname = Fullname;
+		this.Address = Address;
+		this.PhoneNo = PhoneNo;
+	}
 }
